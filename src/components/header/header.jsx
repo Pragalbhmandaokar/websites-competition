5ec21778360dc7284e90852c5a76f3a9c83507a3
viveksgonal <gonalvivek7@gmail.com>
2021-12-27 17:45:18 +0530

    Added Storing functionality

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Navbar from '../navbar/navbar'
-
+import React from "react";
+import Navbar from "../navbar/navbar";
+import {Link} from "react-router-dom";
+import Submission from "../upload/submission"
 export default function header() {
-
-    return (
-        <div className="header"> 
-            <div className="header-main">
-                <div className="box-left"><Navbar></Navbar></div>
-                <div className="box-middle">Competetion</div>
-                <div className="searchBox-container" href="/submission"> 
-                    <button href="/submission">Submit your WEBSITE</button>
-                
-                </div>
-            </div>
-            {/* <div className="box_filter">
+  return (
+    <div className="header">
+      <div className="header-main">
+        <div className="box-left">
+          <Navbar></Navbar>
+        </div>
+        <div className="box-middle">Competetion</div>
+        <div className="searchBox-container" href="/submission">
+            <button onClick={() => <Submission></Submission>}>Submit your WEBSITE</button>
+          
+        </div>
+      </div>
+      {/* <div className="box_filter">
                 <div className="box-filter-main">
                     <ul className="menu-filter">
                         <li className="item">Filter :  </li>
@@ -21,6 +23,6 @@ export default function header() {
                     </ul>
                 </div>
             </div> */}
-        </div>
-    )
+    </div>
+  );
 }
